4d92661ba70411ca3b42c9c6e5b9897159427126
kelvin78226 <rosendokelvin8@gmail.com>
2023-04-04 20:10:12 -0300

    apenas testando coisinhas em java

---
+++ projeto.java
@@ -0,0 +1,10 @@
+/**
+ * projeto
+ */
+public class projeto {
+
+    public static void main(String[] args) {
+        System.out.println("se isso aki de certo eu choro");
+        System.out.println();
+    }
+}
