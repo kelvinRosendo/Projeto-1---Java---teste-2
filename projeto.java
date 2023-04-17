defde260f032187a244b293a15a4f3f68b5e5d8e
kelvin78226 <rosendokelvin8@gmail.com>
2023-04-17 19:56:48 -0300

    apreendendo variaveis - Java

--- projeto.java
+++ projeto.java
@@ -1,10 +1,21 @@
-/**
- * projeto
- */
 public class projeto {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws Exception  {
         System.out.println("se isso aki de certo eu choro");
-        System.out.println();
+    }
+}
+
+//teste de variaveis
+
+class projeto2 {
+
+    public static void main(String[] args) throws Exception {
+        byte numeroPequno = 12 ;
+        short numerosShorts = 32000 ;
+        int numeroMaior = -564 ;
+
+        System.out.println(numeroPequno);
+        System.out.println(numerosShorts);
+        System.out.println(numeroMaior);
     }
 }
