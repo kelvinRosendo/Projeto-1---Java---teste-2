615e6cf07a6988cacb50cb324da76988a490c117
kelvin78226 <rosendokelvin8@gmail.com>
2023-04-21 15:33:13 -0300

    apreendendo masi sobre as variaveis de java

--- projeto.java
+++ projeto.java
@@ -10,12 +10,41 @@ public class projeto {
 class projeto2 {
 
     public static void main(String[] args) throws Exception {
+        
+//variaveis de numeros inteiros: 
         byte numeroPequno = 12 ;
         short numerosShorts = 32000 ;
-        int numeroMaior = -564 ;
+        int numeroMaior = 54564 ;
+        long numeroEnorme = 922337203685477000l;
 
+//variaveis de numeros numeros decimais/floats:
+
+    float pesoPessoal = 56.6f;
+    double Pi = 3.141592656654654;
+
+//variaveis de caracteres 
+
+    char letra = 'a';
+    
+// variavel de False e True (booleano)
+    
+    boolean estouComSede = true;
+    
+// variaveis dp tipo String 
+    
+    String nome = "Kelvin";
+    String texto = "okay java não é tão ruim quanto parece, ainda...";
+
+    //os "prints"
         System.out.println(numeroPequno);
         System.out.println(numerosShorts);
         System.out.println(numeroMaior);
+        System.out.println(numeroEnorme);
+        System.out.println(pesoPessoal);
+        System.out.println(Pi);
+        System.out.println(estouComSede);
+        System.out.println(letra);
+        System.out.println(nome);
+        System.out.println(texto);
     }
 }
